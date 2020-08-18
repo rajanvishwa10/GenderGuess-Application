349f9148c7b9f309934b0e8ab3d0bd4285996bab
rajanvishwa10 <rajanvishwa10@gmail.com>
2020-08-18 21:26:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/genderguessapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.genderguessapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+     Button button;
+     private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.name);
+        textView = findViewById(R.id.gender);
+        button = findViewById(R.id.submt);
+        requestQueue = Volley.newRequestQueue(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = editText.getText().toString();
+                String url = "https://gender-api.com/get?key=RCTNUMwRNLwTSBtmUm&name=";
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url+name, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+
+                                    String gender = response.getString("gender").toUpperCase();
+                                    Log.d("gender",gender);
+                                    textView.setText("Gender of "+name+" : "+gender);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+                requestQueue.add(request);
+            }
+        });
+    }
+}
